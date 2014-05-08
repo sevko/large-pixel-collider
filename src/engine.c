a8fcbf8bcaadb6409785954b2eaad2eec37046bf
Severyn Kozak <severyn.kozak@gmail.com>
2014-05-08 12:52:10 -0400

    Replace graphicscript with MDL files.
    Add, Mod:
    	scripts/((robot, test).mdl, *.gsc)
    		-Add two MDL files used for testing.
    		-Remove old *.gsc files.

--- src/engine.c
+++ src/engine.c
@@ -133,3 +133,16 @@ int main(int argc, char * argv[]){
 	argumentHandler(argc, argv);
 	return EXIT_SUCCESS;
 }
+
+// typedef struct yy_buffer_state * YY_BUFFER_STATE;
+// extern int yyparse();
+// extern YY_BUFFER_STATE yy_scan_string(char * str);
+// extern void yy_delete_buffer(YY_BUFFER_STATE buffer);
+
+// int main(){
+	// char string[] = "torus 0 5 10 100 200";
+	// YY_BUFFER_STATE buffer = yy_scan_string(string);
+	// yyparse();
+	// yy_delete_buffer(buffer);
+	// return 0;
+// }
